b069d20c6a089501a09f6e32f77abab874d28bf0
Alex <i.republic2014@gmail.com>
2018-10-04 18:41:08 +0300

    modifed method main

--- DurakCardDeck/src/durakcarddeck/DurakCardDeck.java
+++ DurakCardDeck/src/durakcarddeck/DurakCardDeck.java
@@ -5,6 +5,8 @@
  */
 package durakcarddeck;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Alex
@@ -15,7 +17,33 @@ public class DurakCardDeck {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String[] suits = new String[]{"Hearts", "Diamonds", "Spades", "Clubs"};
+        String[] names = new String[]{"6", "7", "8", "9",
+            "10", "Jack", "Queen", "King", "Ace"};
+        Scanner scan = new Scanner(System.in);
+        int playerCount = 3;
+        do {
+            System.out.print("Enter the number of players (between 2 and 6): ");
+            playerCount = scan.nextInt(); //show this, while false count of players
+        } while (playerCount < 2 || playerCount > 6);
+        CardGame game = new CardGame(playerCount);
+        game.generateCardDeck(suits, names);
+        System.out.println("Shuffle the deck...");
+        game.shuffleDeck();
+        game.setTrumpSuit();
+        System.out.println("Takes the trump card...");
+        System.out.println("Trump card: " + game.getTrumpCard());
+        System.out.println("Puts the trump card in deck's bottom...");
+        System.out.println("Deals the cards...");
+        game.dealCards();
+        for (int i = 0; i < playerCount; i++) {
+            System.out.println((i + 1) + " player, set:\n"
+                    + game.getPlayersSets()[i]);
+        }
+        CardSet maxSet = game.getMaxSet();
+        System.out.println(String.format("Set with max strength "
+                + "in player %d:\n%s",
+                game.getPlayerNum() + 1, maxSet));
     }
-    
+
 }
